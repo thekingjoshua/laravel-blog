63c09af740d0f1e3e8e76d2baf288b796fc98ad6
KingJoshua <kingjayeriughe@gmail.com>
2025-08-25 19:40:01 +0100

    feat: encapsulate footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,104 @@
+    <footer class="pt-50 pb-20 bg-grey">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="sidebar-widget wow fadeInUp animated mb-30">
+                        <div class="widget-header-2 position-relative mb-30">
+                            <h5 class="mt-5 mb-30">About me</h5>
+                        </div>
+                        <div class="textwidget">
+                            <p>
+                                Start writing, no matter what. The water does not flow until the faucet is turned on.
+                            </p>
+                            <p><strong class="color-black">Address</strong><br>
+                                123 Main Street<br>
+                                New York, NY 10001</p>
+                            <p><strong class="color-black">Follow me</strong><br>
+                                <ul class="header-social-network d-inline-block list-inline color-white mb-20">
+                                    <li class="list-inline-item"><a class="fb" href="#" target="_blank" title="Facebook"><i class="elegant-icon social_facebook"></i></a></li>
+                                    <li class="list-inline-item"><a class="tw" href="#" target="_blank" title="Tweet now"><i class="elegant-icon social_twitter"></i></a></li>
+                                    <li class="list-inline-item"><a class="pt" href="#" target="_blank" title="Pin it"><i class="elegant-icon social_pinterest"></i></a></li>
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <div class="sidebar-widget widget_categories wow fadeInUp animated mb-30" data-wow-delay="0.1s">
+                        <div class="widget-header-2 position-relative mb-30">
+                            <h5 class="mt-5 mb-30">Quick link</h5>
+                        </div>
+                        <ul class="font-small">
+                            <li class="cat-item cat-item-2"><a href="#">About me</a></li>
+                            <li class="cat-item cat-item-4"><a href="#">Help & Support</a></li>
+                            <li class="cat-item cat-item-5"><a href="#">​​Licensing Policy</a></li>
+                            <li class="cat-item cat-item-6"><a href="#">Refund Policy</a></li>
+                            <li class="cat-item cat-item-7"><a href="#">Hire me</a></li>
+                            <li class="cat-item cat-item-7"><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="sidebar-widget widget_tagcloud wow fadeInUp animated mb-30" data-wow-delay="0.2s">
+                        <div class="widget-header-2 position-relative mb-30">
+                            <h5 class="mt-5 mb-30">Tagcloud</h5>
+                        </div>
+                        <div class="tagcloud mt-50">
+                            <a class="tag-cloud-link" href="category.html">beautiful</a>
+                            <a class="tag-cloud-link" href="category.html">New York</a>
+                            <a class="tag-cloud-link" href="category.html">droll</a>
+                            <a class="tag-cloud-link" href="category.html">intimate</a>
+                            <a class="tag-cloud-link" href="category.html">loving</a>
+                            <a class="tag-cloud-link" href="category.html">travel</a>
+                            <a class="tag-cloud-link" href="category.html">fighting </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="sidebar-widget widget_newsletter wow fadeInUp animated mb-30" data-wow-delay="0.3s">
+                        <div class="widget-header-2 position-relative mb-30">
+                            <h5 class="mt-5 mb-30">Newsletter</h5>
+                        </div>
+                        <div class="newsletter">
+                            <p class="font-medium">Subscribe to our newsletter and get our newest updates right on your inbox.</p>
+                            <form class="input-group form-subcriber mt-30 d-flex">
+                                <input type="email" class="form-control bg-white font-small" placeholder="Enter your email">
+                                <button class="btn bg-primary text-white" type="submit">Subscribe</button>
+                                <label class="mt-20"> <input class="mr-5" name="name" type="checkbox" value="1" required=""> I agree to the <a href="#" target="_blank">terms &amp; conditions</a> </label>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copy-right pt-30 mt-20 wow fadeInUp animated">
+                <p class="float-md-left font-small text-muted">© 2020, Stories - Personal Blog HTML Template </p>
+                <p class="float-md-right font-small text-muted">
+                    Design by <a href="https://alithemes.com" target="_blank">AliThemes</a> | All rights reserved
+                </p>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <div class="dark-mark"></div>
+    <!-- Vendor JS-->
+    <script src="{{asset('js/vendor/modernizr-3.6.0.min.js')}} ./assets/"></script>
+    <script src="{{asset('js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('js/vendor/popper.min.js')}}"></script>
+    <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.slicknav.js')}}"></script>
+    <script src="{{asset('js/vendor/slick.min.js')}}"></script>
+    <script src="{{asset('js/vendor/wow.min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.ticker.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.vticker-min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.magnific-popup.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.sticky.js')}}"></script>
+    <script src="{{asset('js/vendor/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('js/vendor/waypoints.min.js')}}"></script>
+    <script src="{{asset('js/vendor/jquery.theia.sticky.js')}}"></script>
+    <!-- NewsBoard JS -->
+    <script src="{{asset('js/main.js')}} "></script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"3aa9a3481f734e94bceb8bb1bd648ba1","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
+</body>
+
+</html>
