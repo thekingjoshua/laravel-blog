94d249ed76a613e5fea53b91b443382801455438
KingJoshua <kingjayeriughe@gmail.com>
2025-08-26 19:19:13 +0100

    feat: add Author Table Seeder

---
+++ database/seeders/AuthorsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AuthorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('authors')->insert([
+            [
+                "name" => "John Doe",
+                "email" => "johndoe@gmail.com",
+                "bio" => "A passionate tech blogger and writer",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Jane Doe",
+                "email" => "Janedoe@gmail.com",
+                "bio" => "Loves writing about fashion and design",
+                "created_at" => now(),
+                "updated_at" => now()
+            ]
+        ]);
+    }
+}
