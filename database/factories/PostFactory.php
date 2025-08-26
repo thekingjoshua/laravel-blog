7ed7c66b2f0bdcff587503bb07969b88f86c26ea
KingJoshua <kingjayeriughe@gmail.com>
2025-08-26 20:08:35 +0100

    feat: randomize author_id in relation to authors table

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -20,7 +20,7 @@ class PostFactory extends Factory
         return [
             "title" => fake()->sentence(12),
             "content" => fake()->paragraph(25),
-            "author_id" => Author::factory()
+            "author_id" => Author::inRandomOrder()->first()->id
         ];
     }
 }
