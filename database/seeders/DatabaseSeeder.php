45568b633047af8a5ca3efbef83930566a9ad0cb
KingJoshua <kingjayeriughe@gmail.com>
2025-08-26 19:18:19 +0100

    update: use Author Factory to seed the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Author::factory(1)->create();
+        // $this->call([
+        //     AuthorsTableSeeder::class
+        // ]);
+        // Author::factory()->create([
+        //     'name' => 'Joshua Erughe',
+        //     'email' => 'joshuaerughe@gmail.com',
+        // ]);
     }
 }
