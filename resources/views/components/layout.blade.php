053eddefd94987a49065a0c7c2711a6d3b4dc4e8
KingJoshua <kingjayeriughe@gmail.com>
2025-08-27 20:45:47 +0100

    feat: use latest comments component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -46,61 +46,8 @@
                             {{-- MOST POPULAR: DISPLAY THE POSTS WITH THE HIGHEST NUMBER OF VIEWS --}}
                 <x-popular-posts/>
                         {{-- END OF POPULAR POSTS --}}
-                            {{-- LATEST COMMENTS --}}
-                            <div class="sidebar-widget widget-latest-posts mb-50 wow fadeInUp animated">
-                                <div class="widget-header-1 position-relative mb-30">
-                                    <h5 class="mt-5 mb-30">Last comments</h5>
-                                </div>
-                                <div class="post-block-list post-module-2">
-                                    <ul class="list-post">
-                                        <li class="mb-30 wow fadeInUp animated">
-                                            <div class="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
-                                                <div class="post-thumb post-thumb-64 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
-                                                    <a class="color-white" href="single.html">
-                                                        <img src="assets/imgs/authors/author-2.jpg" alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="post-content media-body">
-                                                    <p class="mb-10"><a href="author.html"><strong>David</strong></a>
-                                                        <span class="ml-15 font-small text-muted has-dot">16 Jan 2020</span>
-                                                    </p>
-                                                    <p class="text-muted font-small">A writer is someone for whom writing is more difficult than...</p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="mb-30 wow fadeInUp animated">
-                                            <div class="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
-                                                <div class="post-thumb post-thumb-64 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
-                                                    <a class="color-white" href="single.html">
-                                                        <img src="assets/imgs/authors/author-3.jpg" alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="post-content media-body">
-                                                    <p class="mb-10"><a href="author.html"><strong>Kokawa</strong></a>
-                                                        <span class="ml-15 font-small text-muted has-dot">12 Feb 2020</span>
-                                                    </p>
-                                                    <p class="text-muted font-small">Striking pewter studded epaulettes silver zips</p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="wow fadeInUp animated">
-                                            <div class="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
-                                                <div class="post-thumb post-thumb-64 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
-                                                    <a class="color-white" href="single.html">
-                                                        <img src="assets/imgs/news/thumb-1.jpg" alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="post-content media-body">
-                                                    <p class="mb-10"><a href="author.html"><strong>Tsukasi</strong></a>
-                                                        <span class="ml-15 font-small text-muted has-dot">18 May 2020</span>
-                                                    </p>
-                                                    <p class="text-muted font-small">Workwear bow detailing a slingback buckle strap</p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                            {{-- LATEST COMMENTS: DISPLAY THE MOST RECENT COMMENTS MADE ACROSS VARIOUS POSTS --}}
+                            <x-latest-comment/>
                         {{-- END OF LATEST COMMENTS --}}
                         </div>
                     </div>
