9cb7d29d7de6e090abe91c4898d506da326ef37b
KingJoshua <kingjayeriughe@gmail.com>
2025-08-29 20:53:42 +0100

    feat: add GET route to view register page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'register']);
