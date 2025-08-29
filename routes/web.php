836420dba18de0d15dfdbf8f327ab1a8409c7b32
KingJoshua <kingjayeriughe@gmail.com>
2025-08-29 20:44:26 +0100

    feat: add GET route for login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/login', [LoginController::class, 'login']);
