32b92507549661b4ee73a01385c7f00c3acd1a5a
KingJoshua <kingjayeriughe@gmail.com>
2025-09-01 21:16:44 +0100

    feat: add method to handle registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
-{
+{   
     public function register(){
         return view("auth.register");
+    }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            "name" => "required|string|max:100",
+            "email" => "required|string|email|max:100|unique:users",
+            "password" => "required|string|min:8|confirmed"
+        ]);
+
+        $validatedData["password"] = Hash::make($validatedData["password"]);
+
+        $user = User::create($validatedData);
+
     }
 }
