660946dfb36f288d99f6dc7d6257cfbb71bc0f31
KingJoshua <kingjayeriughe@gmail.com>
2025-09-01 22:10:59 +0100

    feat: add authenticate method to handle user login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class LoginController extends Controller
 {
     public function login(): View{
         return view("auth.login");
+    }
+
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            "email" => 'required|string|email|max:100',
+            "password" => 'required|string'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            echo 'valid. Login successful';
+        }else{
+            echo 'invalid. Login not successful';
+        }
     }
 }
